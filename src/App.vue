ac6fe6f538405bf1a12083dd715f1328eb8f794b
Cody Pinkston <c.pinkston2004@gmail.com>
2026-03-08 18:23:32 -0500

    implement watcher for shopping cart items

--- src/App.vue
+++ src/App.vue
@@ -68,7 +68,7 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 let username = 'Harry'
 let shoppingCartItems = ref([
@@ -154,6 +154,11 @@ let taxEstimate = computed(() => subtotal.value *0.08)
 
 let total = computed(() => subtotal.value + shippingEstimate.value + taxEstimate.value)
 
+watch(shoppingCartItems, () =>{
+    localStorage.setItem('hogwartsShoppingCart', JSON.stringify(shoppingCartItems.value))
+},
+{deep: true}
+)
 
 </script>
 
